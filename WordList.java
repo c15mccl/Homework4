5641e121b1ad0acb5dbecc21ee205e6c9d8f96a5
Gage <a31gage@siena.edu>
2019-04-03 14:25:04 -0400

    Second class that keeps track of what words have been found during the game

---
+++ WordList.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class WordList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WordList
+{
+    // instance variables - replace the example below with your own
+    private String word;
+    private boolean wordFound;
+
+    /**
+     * Constructor for objects of class WordList
+     */
+    public WordList(String word)
+    {
+        wordFound = false;
+        this.word = word;
+    }
+    
+    public String getWord()
+    {
+        return word;
+    }
+
+    public void setWordFound(boolean wordFound)
+    {
+        this.wordFound = wordFound;
+    }
+    
+    public boolean getWordFound()
+    {
+        return wordFound;
+    }
+}
